127fab2c915c606e23950568c828b41ea57fc7a4
deionizedoatmeal <ian.k.bania@gmail.com>
2022-05-28 01:34:21 -0700

    added BoM

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
+# Bill of Materials:
+| Part           | Quantity | Package          | Value          | Link                      |
+| -------------- |:--------:|:----------------:|:--------------:|:-------------------------:|
+| Microprocessor | 1        | QFN-56           | RP2040         |                           |
+| 8MB SPI Flash  | 1        | SOIC-8           |                |                           |
+| NeoPixel LED   | 37       | SK6805-EC        | RGB | [Adafruit](adafruit.com/product/4492)|
+| 7 Segement LED | 4        | LTS6760          | Common Cathode |                           | 
+| Button Switch  | 5        | KMR2             | Momentary      |                           |
+| Schottky Diode | 4        | SOD-123          | MBR0540        |                           |
+| Transistor     | 14       | SOT-23           |                |                           |
+| Crystal        | 1        | 2.5mm X 2.0mm    | 12.000 MHz     |                           |
+| I2C Quad ADC   | 8        | SOIC-16          | PCF8591        |                           |
+| Quad Logic Shifter | 1    | SOIC-16          | 74AHCT125      |                           |
+| Voltage Regulator | 4     | SOT-23-5         | AP2112K-3.3    |                           |
+| USB-C Receptacle | 5      | SMD              |                |                           |
+| USB-C Plug     | 4        | SMD Edge Launch  | | [Adafruit](adafruit.com/product/4932) [Digikey](https://www.digikey.com/en/products/detail/adafruit-industries-llc/4932/13997774) |
+| -------------- |:--------:|:----------------:|:--------------:|:-------------------------:|
+| Resistor       | 23       | 0603/1608 Metric | 300 Ω          |                           |
+| Resistor       | 10       | 0603/1608 Metric | 1 kΩ           |                           |
+| Resistor       | 2        | 0603/1608 Metric | 5.11 kΩ        |                           |
+| Resistor       | 60       | 0603/1608 Metric | 10 kΩ          |                           |
+| Resistor       | 9        | 0603/1608 Metric | 1 MΩ           |                           |
+| -------------- |:--------:|:----------------:|:--------------:|:-------------------------:|
+| Resistor       | 2        | 0402/1005 Metric | 27 Ω           |                           |
+| -------------- |:--------:|:----------------:|:--------------:|:-------------------------:|
+| Capacitor      | 45       | 0805/2012 Metric | 0.1 µF         |                           |
+| Capacitor      | 20       | 0085/2012 Metric | 10 µF          |                           |
+| -------------- |:--------:|:----------------:|:--------------:|:-------------------------:|
+| Capacitor      | 2        | 0402/1005 Metric | 27 pF          |                           |
+| Capacitor      | 10       | 0402/1005 Metric | 100 nF         |                           |
+| Capacitor      | 2        | 0402/1005 Metric | 1 µF           |                           |
+
+# 3D Printing Pieces:
+
+# Assembly Instructions:
+
+# Software Install Instructions:
+1) Hold down boot button on underside while plugging USB into computer.
+2) When the RP2040 device volume shows up, copy Adafruit's CircuitPython library to
+3) Wait for the device to disappear and reappear, then copy all the libraries into the lib/ directory on the RP2040.
+4) Finally, copy `code.py` onto the RP2040.
+
+# Images
 ![PCB](/full_board_render/full_board.png)
--- RP2040_code/code.py
+++ RP2040_code/code.py
@@ -13,215 +13,13 @@ import analogio
 import adafruit_pcf8591.pcf8591 as PCF
 from adafruit_pcf8591.analog_in import AnalogIn
 from adafruit_pcf8591.analog_out import AnalogOut
-
-##################################
-### GLOBALS AND INITIALIZATION ###
-##################################
-# define RGB colors to use
-# feel free to change these if you want
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-purple = (255, 0, 255)
-yellow = (255, 255, 0)
-orange = (255, 165, 0)
-white = (255, 255, 255)
-none = (0, 0, 0)
-
-# define LED brightness, 0 -> 1 float, feel free to change
-brightness = 0.3
-
-# RP2040 voltage calibration factor
-RPv = 3.3/65535
-
-# PCF8591 voltage calibration factor
-PCFv = 3.3/65535
-
-# define the RGB LEDs
-sup_pixels = neopixel.NeoPixel(board.GP6, 34)
-season_pixels = neopixel.NeoPixel(board.GP2, 3)
-
-# define the buttons
-next_season = digitalio.DigitalInOut(board.GP4)
-next_season.switch_to_input(pull=digitalio.Pull.DOWN)
-
-prev_season = digitalio.DigitalInOut(board.GP5)
-prev_season.switch_to_input(pull=digitalio.Pull.DOWN)
-
-restart = digitalio.DigitalInOut(board.GP3)
-restart.switch_to_input(pull=digitalio.Pull.DOWN)
-
-# define the analog pins
-sup5 =  analogio.AnalogIn(board.GP26)
-sup6 =  analogio.AnalogIn(board.GP27)
-sup34 =  analogio.AnalogIn(board.GP28)
-
-# set up ADCs using I2C logic pins
-i2c = busio.I2C(scl=board.GP1, sda=board.GP0)
-
-# first PCF8591 IC (top L board)
-ADC_1 = PCF.PCF8591(i2c, 0x48)
-sup1 = AnalogIn(ADC_1, PCF.A0)
-sup2 = AnalogIn(ADC_1, PCF.A1)
-sup3 = AnalogIn(ADC_1, PCF.A2)
-sup4 = AnalogIn(ADC_1, PCF.A3)
-
-# second PCF8591 IC (top R board)
-ADC_2 = PCF.PCF8591(i2c, 0x49)
-sup7 = AnalogIn(ADC_2, PCF.A0)
-sup8 = AnalogIn(ADC_2, PCF.A1)
-sup9 = AnalogIn(ADC_2, PCF.A2)
-
-# third PCF8591 IC (bottom L board)
-ADC_3 = PCF.PCF8591(i2c, 0x4A)
-sup10 = AnalogIn(ADC_3, PCF.A0)
-sup11 = AnalogIn(ADC_3, PCF.A1)
-sup12 = AnalogIn(ADC_3, PCF.A2)
-sup13 = AnalogIn(ADC_3, PCF.A3)
-
-# forth PCF8591 IC (bottom L board)
-ADC_4 = PCF.PCF8591(i2c, 0x4C)
-sup14 = AnalogIn(ADC_4, PCF.A0)
-sup15 = AnalogIn(ADC_4, PCF.A1)
-sup16 = AnalogIn(ADC_4, PCF.A2)
-sup17 = AnalogIn(ADC_4, PCF.A3)
-
-# fifth PCF8591 IC (bottom L board)
-ADC_5 = PCF.PCF8591(i2c, 0x4B)
-sup18 = AnalogIn(ADC_5, PCF.A0)
-sup19 = AnalogIn(ADC_5, PCF.A1)
-sup20 = AnalogIn(ADC_5, PCF.A2)
-sup21 = AnalogIn(ADC_5, PCF.A3)
-
-# sixth PCF8591 IC (bottom R board)
-ADC_6 = PCF.PCF8591(i2c, 0x4D)
-sup22 = AnalogIn(ADC_6, PCF.A0)
-sup23 = AnalogIn(ADC_6, PCF.A1)
-sup24 = AnalogIn(ADC_6, PCF.A2)
-sup25 = AnalogIn(ADC_6, PCF.A3)
-
-# seventh PCF8591 IC (bottom R board)
-ADC_7 = PCF.PCF8591(i2c, 0x4E)
-sup26 = AnalogIn(ADC_7, PCF.A0)
-sup27 = AnalogIn(ADC_7, PCF.A1)
-sup28 = AnalogIn(ADC_7, PCF.A2)
-sup29 = AnalogIn(ADC_7, PCF.A3)
-
-# eighth PCF8591 IC (bottom R board)
-ADC_8 = PCF.PCF8591(i2c, 0x4F)
-sup30 = AnalogIn(ADC_8, PCF.A0)
-sup31 = AnalogIn(ADC_8, PCF.A1)
-sup32 = AnalogIn(ADC_8, PCF.A2)
-sup33 = AnalogIn(ADC_8, PCF.A3)
-
-supply_center_TPs = [sup1, sup2, sup3, sup4, sup5, sup6, sup7, sup8, sup9, sup10, sup11, sup12, sup13, sup14, sup15, sup16, sup17, sup18, sup19, sup20, sup21, sup22, sup23, sup24, sup25, sup26, sup27, sup28, sup29, sup30, sup31, sup32, sup33, sup34]
-
-# define the supply center LEDs
-Edi = sup_pixels[0]
-Lvp = sup_pixels[1]
-Lon = sup_pixels[2]
-Nwy = sup_pixels[3]
-Den = sup_pixels[4]
-Kie = sup_pixels[5]
-Swe = sup_pixels[6]
-StP = sup_pixels[7]
-Mos = sup_pixels[8]
-War = sup_pixels[9]
-Sev = sup_pixels[10]
-Ank = sup_pixels[11]
-Con = sup_pixels[12]
-Smy = sup_pixels[13]
-Gre = sup_pixels[14]
-Bul = sup_pixels[15]
-Rum = sup_pixels[16]
-Ser = sup_pixels[17]
-Try = sup_pixels[18]
-Bud = sup_pixels[19]
-Ven = sup_pixels[20]
-Nap = sup_pixels[21]
-Rom = sup_pixels[22]
-Tun = sup_pixels[23]
-Mar = sup_pixels[24]
-Spa = sup_pixels[25]
-Por = sup_pixels[26]
-Bre = sup_pixels[27]
-Par = sup_pixels[28]
-Bel = sup_pixels[29]
-Ber = sup_pixels[30]
-Mun = sup_pixels[31]
-Ven = sup_pixels[32]
-Hol = sup_pixels[33]
-
-# list holding all the neopixel objects
-supply_center_LEDs = [Edi, Lvp, Lon, Nwy, Den, Kie, StP, MoS, War, Sev, Ank, Con, Smy, Gre, Bul, Rum]
-
-# set 7 segement display pins
-tens_A = digitalio.DigitalInOut(board.GP7)
-tens_A.direction = digitalio.Direction.OUTPUT
-
-tens_B = digitalio.DigitalInOut(board.GP8)
-tens_B.direction = digitalio.Direction.OUTPUT
-
-tens_C = digitalio.DigitalInOut(board.GP9)
-tens_C.direction = digitalio.Direction.OUTPUT
-
-tens_D = digitalio.DigitalInOut(board.GP10)
-tens_D.direction = digitalio.Direction.OUTPUT
-
-tens_E = digitalio.DigitalInOut(board.GP11)
-tens_E.direction = digitalio.Direction.OUTPUT
-
-tens_F = digitalio.DigitalInOut(board.GP12)
-tens_F.direction = digitalio.Direction.OUTPUT
-
-tens_G = digitalio.DigitalInOut(board.GP13)
-tens_G.direction = digitalio.Direction.OUTPUT
-
-ones_A = digitalio.DigitalInOut(board.GP14)
-ones_A.direction = digitalio.Direction.OUTPUT
-
-ones_B = digitalio.DigitalInOut(board.GP15)
-ones_B.direction = digitalio.Direction.OUTPUT
-
-ones_C = digitalio.DigitalInOut(board.GP16)
-ones_C.direction = digitalio.Direction.OUTPUT
-
-ones_D = digitalio.DigitalInOut(board.GP17)
-ones_D.direction = digitalio.Direction.OUTPUT
-
-ones_E = digitalio.DigitalInOut(board.GP18)
-ones_E.direction = digitalio.Direction.OUTPUT
-
-ones_F = digitalio.DigitalInOut(board.GP19)
-ones_F.direction = digitalio.Direction.OUTPUT
-
-ones_G = digitalio.DigitalInOut(board.GP20)
-ones_G.direction = digitalio.Direction.OUTPUT
-
-# initalize season and year counting
-# counter % 3 is the season, 1 = spring, 2 = fall, 0 = winter
-# counter // 3 is the year, 0 = 1901, 1 = 1902, etc
-counter = 1
-
-# initalize current color list and set all LEDs to off
-sup_LEDs_off()
-current_list = []
-for i in range(34):
-    current_LED.append(none)
-
-# initalize turn history
-# turn history is list of lists: [[counter1, [color1, color2, color3, ...]],  [counter2, [color1, color2, color3, ...]], ... ]
-history = [counter, current_list]
+from random import randrange
 
 #################
 ### FUNCTIONS ###
 #################
-def set_7seg_disp(year: int):
+def set_7seg_disp(first_digit: int, second_digit: int):
     """writes the proper year to the 7 seg displays"""
-    actual_year = year + 1
-    tens_digit = str(actual_year)[0]
-    ones_digit = str(actual_year)[1]
-
     # set the component LEDs of 7 segment display
     if second_digit == 0:
         ones_A = True
@@ -404,50 +202,44 @@ def set_7seg_disp(year: int):
         tens_G = True 
     return
 
-def reset_history():
-    """clears all past turn history"""
-    history = [] 
-    history = [counter, current_list]
-    return
-
 def fall_sup_check():
     """updates the LEDs of any supply centers who are ocupied by a color than what the show"""
-    for i in range(len(supply_center_LEDs)):
+    for i in range(len(sup_pixels)):
         # color the supply center currently is
-        color = get_sup_color(supply_center_LEDs[i], i)
+        color = old_current_LED[i]
 
-        # piece currently occupying supply center 
+        # color of piece currently occupying supply center 
         piece = get_sup_piece(supply_center_TPs[i])
+        
+        # create a new list
+        current_LED[i] = piece
 
         if color == piece or piece == none:
             # no ownership change, check next LED
             continue
         elif color != piece:
             # onwership has changed, set color to match
-            set_sup_color(supply_center_LEDs[i], piece)
+            sup_pixels[i] = piece
     return
 
-def get_sup_color(sup_center, i):
-    """retrieves the color a supply center is currently showing"""
-    # returns: blue, green, red, orange, purple, white, yellow, none
-    return current_list[i]
-
 def get_sup_piece(sup_center):
-    """retrieves the color the piece on a supply center"""
+    """uses ADC to retrieve the color the piece on a supply center"""
     # returns color based on voltage, voltages are:
-    # 0.000 V -> none
-    # 0.471 V -> red
-    # 0.943 V -> green
-    # 1.414 V -> blue
-    # 1.886 V -> purple 
-    # 2.357 V -> yellow
-    # 2.829 V -> orange 
-    # 3.300 V -> white 
+    # 0.000 V -> none   => 1 MΩ
+    # 0.471 V -> red    => 60 kΩ 
+    # 0.943 V -> green  => 25 kΩ
+    # 1.414 V -> blue   => 13.4 kΩ
+    # 1.886 V -> purple => 7.5 kΩ
+    # 2.357 V -> yellow => 4 kΩ
+    # 2.829 V -> orange => 1.7 kΩ
+    # 3.300 V -> white  => 0 Ω
     # get voltage
     if sup_center == sup5 or sup_center == sup6 or sup_center == sup34:
-        voltage = RPv * sup_center
+        # AVG over time!!!
+        voltage = RPv * sup_center.value * sup_center.reference_voltage
     else:
-        voltage = PCFv * sup_center
+        # AVG over time!!!
+        voltage = PCFv * sup_center.value * sup_center.reference_voltage
 
     # use voltage to get color
     if voltage < min_voltage:
@@ -469,112 +261,287 @@ def get_sup_piece(sup_center):
     else:
         return "error"
 
-def set_sup_color(sup_center, color):
-    """sets the supply center LED a certain color"""
-    # neopixel library makes this easy
-    sup_center(color) 
-    # sup_pixels.show()
-    return
 
-def season_LED_on(season):
-    """sets the season LED to white"""
-    # neopixel library makes this easy
-    season(white) 
-    # sup_pixels.show()
-    return
+##################################
+### GLOBALS AND INITIALIZATION ###
+##################################
+# define RGB colors to use
+# feel free to change these if you want
+red = (255, 0, 15)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+purple = (255, 0, 255)
+yellow = (255, 255, 0)
+orange = (230, 99, 0)
+# adjusted for specific LEDs
+white = (200, 255, 200)
+none = (0, 0, 0)
 
-def season_LED_off(season):
-    """sets the season LED to none"""
-    # neopixel library makes this easy
-    season(none)
-    # sup_pixels.show()
-    return
+colors = [red, green, blue, purple, yellow, orange, white]
 
-def sup_LEDs_off():
-    """sets all supply center LEDs to none"""
-    sup_pixels.fill(none)
-    return
+# define LED brightness, 0 -> 1 float, feel free to change
+brightness = 0.1
+
+# min
+min_voltage = 0.2
+
+# RP2040 voltage calibration factor
+RPv = 1/65535
+
+# PCF8591 voltage calibration factor
+PCFv = 1/65535
+
+# define the RGB LEDs
+sup_pixels = neopixel.NeoPixel(board.GP6, 34)
+season_pixels = neopixel.NeoPixel(board.GP2, 3)
+sup_pixels.brightness = brightness
+season_pixels.brightness = brightness
+
+# define the buttons
+next_season = digitalio.DigitalInOut(board.GP4)
+next_season.switch_to_input(pull=digitalio.Pull.DOWN)
+
+prev_season = digitalio.DigitalInOut(board.GP5)
+prev_season.switch_to_input(pull=digitalio.Pull.DOWN)
+
+restart = digitalio.DigitalInOut(board.GP3)
+restart.switch_to_input(pull=digitalio.Pull.DOWN)
+
+# define the analog pins
+sup5 =  analogio.AnalogIn(board.GP26)
+sup6 =  analogio.AnalogIn(board.GP27)
+sup34 =  analogio.AnalogIn(board.GP28)
+
+# set up ADCs using I2C logic pins
+i2c = busio.I2C(scl=board.GP1, sda=board.GP0)
+
+# first PCF8591 IC (top L board)
+ADC_1 = PCF.PCF8591(i2c, 0x48)
+sup1 = AnalogIn(ADC_1, PCF.A0)
+sup2 = AnalogIn(ADC_1, PCF.A1)
+sup3 = AnalogIn(ADC_1, PCF.A2)
+sup4 = AnalogIn(ADC_1, PCF.A3)
+
+# second PCF8591 IC (top R board)
+ADC_2 = PCF.PCF8591(i2c, 0x49)
+sup7 = AnalogIn(ADC_2, PCF.A0)
+sup8 = AnalogIn(ADC_2, PCF.A1)
+sup9 = AnalogIn(ADC_2, PCF.A2)
+
+# third PCF8591 IC (bottom L board)
+ADC_3 = PCF.PCF8591(i2c, 0x4A)
+sup10 = AnalogIn(ADC_3, PCF.A0)
+sup11 = AnalogIn(ADC_3, PCF.A1)
+sup12 = AnalogIn(ADC_3, PCF.A2)
+sup13 = AnalogIn(ADC_3, PCF.A3)
+
+# forth PCF8591 IC (bottom L board)
+ADC_4 = PCF.PCF8591(i2c, 0x4C)
+sup14 = AnalogIn(ADC_4, PCF.A0)
+sup15 = AnalogIn(ADC_4, PCF.A1)
+sup16 = AnalogIn(ADC_4, PCF.A2)
+sup17 = AnalogIn(ADC_4, PCF.A3)
+
+# fifth PCF8591 IC (bottom L board)
+ADC_5 = PCF.PCF8591(i2c, 0x4B)
+sup18 = AnalogIn(ADC_5, PCF.A0)
+sup19 = AnalogIn(ADC_5, PCF.A1)
+sup20 = AnalogIn(ADC_5, PCF.A2)
+sup21 = AnalogIn(ADC_5, PCF.A3)
+
+# sixth PCF8591 IC (bottom R board)
+ADC_6 = PCF.PCF8591(i2c, 0x4D)
+sup22 = AnalogIn(ADC_6, PCF.A0)
+sup23 = AnalogIn(ADC_6, PCF.A1)
+sup24 = AnalogIn(ADC_6, PCF.A2)
+sup25 = AnalogIn(ADC_6, PCF.A3)
+
+# seventh PCF8591 IC (bottom R board)
+ADC_7 = PCF.PCF8591(i2c, 0x4E)
+sup26 = AnalogIn(ADC_7, PCF.A0)
+sup27 = AnalogIn(ADC_7, PCF.A1)
+sup28 = AnalogIn(ADC_7, PCF.A2)
+sup29 = AnalogIn(ADC_7, PCF.A3)
+
+# eighth PCF8591 IC (bottom R board)
+ADC_8 = PCF.PCF8591(i2c, 0x4F)
+sup30 = AnalogIn(ADC_8, PCF.A0)
+sup31 = AnalogIn(ADC_8, PCF.A1)
+sup32 = AnalogIn(ADC_8, PCF.A2)
+sup33 = AnalogIn(ADC_8, PCF.A3)
+
+supply_center_TPs = [sup1, sup2, sup3, sup4, sup5, sup6, sup7, sup8, sup9, sup10, sup11, sup12, sup13, sup14, sup15, sup16, sup17, sup18, sup19, sup20, sup21, sup22, sup23, sup24, sup25, sup26, sup27, sup28, sup29, sup30, sup31, sup32, sup33, sup34]
+
+# set 7 segement display pins
+tens_A = digitalio.DigitalInOut(board.GP7)
+tens_A.direction = digitalio.Direction.OUTPUT
+
+tens_B = digitalio.DigitalInOut(board.GP8)
+tens_B.direction = digitalio.Direction.OUTPUT
+
+tens_C = digitalio.DigitalInOut(board.GP9)
+tens_C.direction = digitalio.Direction.OUTPUT
+
+tens_D = digitalio.DigitalInOut(board.GP10)
+tens_D.direction = digitalio.Direction.OUTPUT
+
+tens_E = digitalio.DigitalInOut(board.GP11)
+tens_E.direction = digitalio.Direction.OUTPUT
+
+tens_F = digitalio.DigitalInOut(board.GP12)
+tens_F.direction = digitalio.Direction.OUTPUT
+
+tens_G = digitalio.DigitalInOut(board.GP13)
+tens_G.direction = digitalio.Direction.OUTPUT
+
+ones_A = digitalio.DigitalInOut(board.GP14)
+ones_A.direction = digitalio.Direction.OUTPUT
+
+ones_B = digitalio.DigitalInOut(board.GP15)
+ones_B.direction = digitalio.Direction.OUTPUT
+
+ones_C = digitalio.DigitalInOut(board.GP16)
+ones_C.direction = digitalio.Direction.OUTPUT
+
+ones_D = digitalio.DigitalInOut(board.GP17)
+ones_D.direction = digitalio.Direction.OUTPUT
+
+ones_E = digitalio.DigitalInOut(board.GP18)
+ones_E.direction = digitalio.Direction.OUTPUT
+
+ones_F = digitalio.DigitalInOut(board.GP19)
+ones_F.direction = digitalio.Direction.OUTPUT
+
+ones_G = digitalio.DigitalInOut(board.GP20)
+ones_G.direction = digitalio.Direction.OUTPUT
+
+# initalize season and year counting
+# counter % 3 is the season, 1 = spring, 2 = fall, 0 = winter
+# counter // 3 is the year, 0 = 1901, 1 = 1902, etc
+counter = 0
+
+# calc year and season
+season = counter % 3
+year = counter // 3
+
+# initalize current color list and set all LEDs to off
+sup_pixels.fill(none)
+for i in range(len(sup_pixels)):
+    sup_pixels[i] = colors[randrange(7)]
+
+# current color list initiation
+current_LED = []
+for i in range(34):
+    current_LED.append(none)
+
+# initalize turn history
+# turn history is list of lists: [[counter1, [color1, color2, color3, ...]],  [counter2, [color1, color2, color3, ...]], ... ]
+history = [counter, current_LED]
+
+# initalize toggle variable
+toggle = False
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
+led.value = True
+
 
 #################
 ### MAIN LOOP ###
 #################
 while True:
-    led.value = True
+    # set date displays
+    ones = str(1901 + year)[3]
+    tens = str(1901 + year)[2]
+    set_7seg_disp(tens, ones)
 
-    # check year and season
-    season = counter % 3
-    year = counter // 3
-
-    # set displays
-    set_7seg_disp(year)
+    # set the appropriate season indicator turn off the others
+    season_pixels[(season)%3] = white
+    season_pixels[(season - 1)%3] = none
+    season_pixels[(season - 2)%3] = none 
 
     # if the restart button is toggled
     if restart.value:
-        # reset the counter (this resets the date display
-        counter = 1
+        # check if we've been thru this loop this toggle
+        if toggle == True:
+            continue
 
-        # reset the season displays
-        set_season_on(season_pixels[season])
-        set_season_off(season_pixels[(season - 1)%3])
-        set_season_off(season_pixels[(season - 2)%3])
+        # if first time in the loop set the toggle variable
+        toggle = True
+
+        # reset the counter (this resets the date display
+        counter = 0
+        season = counter % 3
+        year = counter // 3
 
         # turn supply center LEDs off
-        sup_LEDs_off()
+        sup_pixels.fill(none)
 
         # reset the current color list
-        current_list = []
+        current_LED = []
         for i in range(34):
             current_LED.append(none)
 
         # clear and reset the turn history list
-        reset_history()
+        history = [] 
+        history = [counter, current_LED]
+
+        # ignore any coincidental press of next and prev
+        continue
 
     # if the next button is toggled
     if next_season.value:
+        # check if we've been thru this loop this toggle
+        if toggle == True:
+            continue
+
+        # if first time in the loop set the toggle variable
+        toggle = True
+
         # increment the season
         counter = counter + 1
+        season = counter % 3
+        year = counter // 3
         
-        # set the appropriate season indicator
-        set_season_on(season_pixels[season])
-
-        # turn off the other idicators
-        set_season_off(season_pixels[(season - 1)%3])
-        set_season_off(season_pixels[(season - 2)%3])
-
         # if we are moving fall -> winter
-        if season == 1:
+        if season == 2:
+            old_current_LED = current_LED
+
             # check supply centers to see if their ownership has changed
             fall_sup_check()
 
             # add old current list to history
-            history.append([counter, current_list])
+            history.append([counter, old_current_LED])
 
-            # update the current list
-            current_list = []
-            for i in range(34):
-                current_LED.append(none)
+        # ignore any coincidental press of prev
+        continue
             
     # if the previous button is toggled
     if prev_season.value:
+        # check if we've been thru this loop this toggle
+        if toggle == True:
+            continue
+
+        # if first time in the loop set the toggle variable
+        toggle = True
+
         # decrement the season
         counter = counter - 1
-        # set the appropriate season indicator
-        set_season_on(season_pixels[season])
-
-        # turn off the other idicators
-        set_season_off(season_pixels[(season - 1)%3])
-        set_season_off(season_pixels[(season - 2)%3])
+        season = counter % 3
+        year = counter // 3
 
         # check to see if we need to regress the supply centers 
         if season == 1:
             # then get the supply center colors for this value in the list
             for i in range(len(history[-1][1])):
-                set_sup_color(supply_center_LEDs[i], history[-1][1][i])
+                sup_pixels[i] = history[-1][1][i]
 
             # now remove this value from history
             history.pop()
+        continue
+
+    # if no button is depressed
+    else:
+        toggle = False
         
 
